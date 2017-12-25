e08f61b5c4a5e47d5ee6324334a3cdb46b8786ad
oi10536 <34789129+oi10536@users.noreply.github.com>
2017-12-25 10:57:23 +0700

    Update about.sh

--- API/about.sh
+++ API/about.sh
@@ -2,11 +2,11 @@
 #About this script
 
 clear
-figlet "THAI-VPN"
+figlet "TONZA-VPN"
 echo -e "Script Auto Install SSH & OpenVPN V2.0" 
 echo -e ""
 echo -e "For Debian 7 32 bit & 64 bit"
 echo -e "For VPS with KVM and VMWare virtualization"
 echo -e ""
-echo -e "Mod by THAI-VPN"
+echo -e "Mod by TONZA-VPN"
 echo -e ""
